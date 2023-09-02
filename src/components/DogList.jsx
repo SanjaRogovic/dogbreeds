a9294d780ea81a37b1eea8bcaec41474a5e44586
Sanja <74556703+SanjaRogovic@users.noreply.github.com>
2023-09-02 18:46:58 +0200

    added spinner to homepage and doglist

--- src/components/DogList.jsx
+++ src/components/DogList.jsx
@@ -3,38 +3,46 @@ import { useParams } from 'react-router-dom'
 import * as contentful from "contentful";
 import Card from 'react-bootstrap/Card';
 import Rating from '@mui/material/Rating';
+import * as ReactBootstrap from "react-bootstrap";
 
 const DogList = (props) => {
   const [name, setName] = useState("");
   const [img, setImg] = useState("");
   const [about, setAbout] = useState("");
-  const [ratingSport, setRatingSport] = useState(0)
-  const [affectionLevel, setAffectionLevel] = useState(0)
+  const [ratingSport, setRatingSport] = useState(0);
+  const [affectionLevel, setAffectionLevel] = useState(0);
+  const [loading, setLoading] = useState(false);
 
   const { index } = useParams();
 
-
-  const space = import.meta.env.VITE_SOME_SPACE;
-  const accessToken = import.meta.env.VITE_SOME_TOKEN;
+  // const space = import.meta.env.VITE_SOME_SPACE;
+  // const accessToken = import.meta.env.VITE_SOME_TOKEN;
 
   const dogFetch = async () => {
     const client = contentful.createClient({
-      space: space,
-      accessToken: accessToken,
+      space: "y5d9wlvfzf96",
+      accessToken: "oy4sUQfFIhCEhDjJJarewqCr9d5lSi9PiooSKDPAP0U",
     });
 
     const response = await client.getEntry(index);
+    setLoading(true)
     setName(response.fields.breedName);
     setImg(response.fields.dogImg[1].fields.file.url);
     setAbout(response.fields.dogAbout);
-    setRatingSport(response.fields.sportNeed)
-    setAffectionLevel(response.fields.affectionateLevel)
+    setRatingSport(response.fields.sportNeed);
+    setAffectionLevel(response.fields.affectionateLevel);
+
+    if(loading === true){
+      console.error(`Fetching data failed`)
+    } else {
+      setLoading(false)
+    }
+    
   };
 
   useEffect(() => {
     dogFetch();
   }, []);
-
 
   return (
     <div>
@@ -54,7 +62,7 @@ const DogList = (props) => {
       </Card>
     </div>
   );
-}
+};
 
 export default DogList
 
--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -3,59 +3,64 @@ import { useState, useEffect } from "react";
 import * as contentful from "contentful";
 import { Link } from 'react-router-dom'
 import Card  from 'react-bootstrap/Card'
+import * as ReactBootstrap from "react-bootstrap";
 
 
 
 const Homepage = () => {
-    const [breeds, setBreeds] = useState([]);
+  const [breeds, setBreeds] = useState([]);
+  const [loading, setLoading] = useState(false);
 
-    const space = import.meta.env.VITE_SOME_SPACE;
-    const accessToken = import.meta.env.VITE_SOME_TOKEN;
+  // const space = import.meta.env.VITE_SOME_SPACE;
+  // const accessToken = import.meta.env.VITE_SOME_TOKEN;
 
+  const getDogs = async () => {
+    const client = contentful.createClient({
+      space: "y5d9wlvfzf96",
+      accessToken: "oy4sUQfFIhCEhDjJJarewqCr9d5lSi9PiooSKDPAP0U",
+    });
 
-    const getDogs = async () => {
-        const client = contentful.createClient({
-          space: space,
-          accessToken: accessToken,
-        });
-    
-        const response = await client.getEntries()
-        setBreeds(response.items)
-      }
-        
-      useEffect( () => {
-        getDogs()
-        }, []);
-        // console.log(breeds)
+    const response = await client.getEntries();
+    setLoading(true);
+    setBreeds(response.items);
+    setLoading(false)
+  };
+
+  useEffect(() => {
+    getDogs();
+  }, []);
+  // console.log(breeds)
 
   return (
     <>
       <h1>Dog Breeds</h1>
-<div className='cardContainer'> 
-      {breeds.map((breeds, index) => {
-        return (
-          <Card style={{ width: "18rem" }} key={index}>
-            <Card.Body>
-              <Card.Title>
-              <Link key={index} to={`/doglist/${breeds.sys.id}`}>
-              {breeds.fields.breedName}<br />
-              </Link>
-              </Card.Title>
-             
-              <Card.Img
-              variant="bottom" className='cardImg'
-              src={breeds.fields.dogImg[0].fields.file.url}
-            />
-              
-            </Card.Body>
-          </Card>
-        );
-      })}
-
-</div>
+      {loading && (
+          <ReactBootstrap.Spinner animation="border" variant="secondary" />
+        )}
+      <div className="cardContainer">     
+        {breeds.map((breeds, index) => {
+          return (
+            <Card style={{ width: "18rem" }} key={index}>
+              <Card.Body>
+                <Card.Title>
+                  <Link key={index} to={`/doglist/${breeds.sys.id}`}>
+                    {breeds.fields.breedName}
+                    <br />
+                  </Link>
+                </Card.Title>
+                <Card.Img
+                  variant="bottom"
+                  className="cardImg"
+                  src={breeds.fields.dogImg[0].fields.file.url}
+                />
+              </Card.Body>
+            </Card>
+          );
+        })}
+      </div>
     </>
   );
-}
+};
 
 export default Homepage
 
